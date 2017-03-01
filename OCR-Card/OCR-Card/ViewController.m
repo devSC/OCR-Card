080b8f376e8ac70a7485df72afedc696980a8ec4
Wilson Yuan <yuansc@nihao.com>
2017-03-01 17:42:38 +0800

    initial

---
+++ OCR-Card/OCR-Card/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OCR-Card
+//
+//  Created by Wilson Yuan on 2017/3/1.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
